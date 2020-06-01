e20a1ac10d1f5ee76c4c0e8d471540e623a6b30b
mediavil <meekeboose@gmail.com>
2020-06-01 08:14:26 +0800

    react icons

---
+++ src/constants/social-icons.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { FaFacebook, FaTwitterSquare, FaSquareSpace } from "react-icons/fa"
+
+export default [
+  {
+    icon: <FaFacebook />,
+    url: "https:/twitter.com",
+  },
+  {
+    icon: <FaTwitterSquare />,
+    url: "https:/twitter.com",
+  },
+  {
+    icon: <FaSquareSpace />,
+    url: "https:/twitter.com",
+  },
+]
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import { FaAdobe } from "react-icons/fa"
 
 export default function Home() {
   return (
     <Layout>
       <div>Hello from backroads</div>
+      <FaAdobe />
     </Layout>
   )
 }
